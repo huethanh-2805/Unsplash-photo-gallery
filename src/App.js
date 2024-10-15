17f7182126a8188fb7476b56c1c879d61bfb7a92
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2024-10-16 00:07:04 +0700

    Edit deploy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import PhotoGrid from "./components/PhotoGrid";
 import PhotoDetails from "./components/PhotoDetails";
 
 const App = () => {
-    return (
+  return (
+   
       <div>
         <Routes>
-            {/* Chuyển hướng từ '/' đến '/photos' */}
-            <Route path="/" element={<Navigate to="/photos" />} /> 
-            <Route path="/photos" element={<PhotoGrid/>}></Route>
-            <Route path="/photos/:id" element={<PhotoDetails/>}></Route>
+          <Route path="/" element={<Navigate to="/photos" />} />
+          <Route path="/Unsplash-photo-gallery/photos" element={<PhotoGrid />} />
+          <Route path="/Unsplash-photo-gallery/photos/:id" element={<PhotoDetails />} />
         </Routes>
-      </div>  
-    );
+      </div>
+
+  );
 };
 
 export default App;
